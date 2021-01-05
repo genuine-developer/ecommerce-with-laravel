be1455b7b42582dc411b2643f995450bfa16084b
intrepid.arctic.scavenger <intrepid.arctic.scavenger@gmail.com>
2021-01-05 02:31:23 +0100

    CartController Added

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    /**
+     * Add To Cart
+     */
+    function AddToCart(Request $request){
+        return $request;
+    }
+}
