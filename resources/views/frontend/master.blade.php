c390342646c5c189dc1faba1bec86582cccd74e6
intrepid.arctic.scavenger <intrepid.arctic.scavenger@gmail.com>
2021-02-03 22:31:48 +0100

    Tawk to live chat added

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -359,6 +359,19 @@
 
 
     @yield('footer_js')
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/601b145fc31c9117cb7594f0/1etksf6n5';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
